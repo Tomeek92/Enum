875612c9c00d58fac2a2a640ce2a73f0ce360155
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2023-10-02 12:59:31 +0200

    Dodaj pliki projektów.

---
+++ Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿class Sport
+{
+    enum Pilka
+    {
+        Reczna,Nozna,Siatkowa,Koszykowka
+    }
+
+    enum SprzetSportowy
+    {
+        Kij,Pilka,Lotki,Paletki,Rakiety
+    }
+    enum Wyniki
+    {
+        Gole=10,
+        Mecze=15,
+        ZolteKartki=7,
+        
+    }
+    static int razem = (int)Wyniki.Gole + (int)Wyniki.Mecze + (int)Wyniki.ZolteKartki;
+    
+
+    static void Main(string[] args)
+    {
+        Pilka pilka = Pilka.Reczna;
+        
+        Console.WriteLine(SprzetSportowy.Pilka);
+        Console.WriteLine(Pilka.Reczna);
+        Console.WriteLine($"Wynikiem całościowy z meczami, golami oraz żółtymi kartkami {razem}");
+    }
+
+}
